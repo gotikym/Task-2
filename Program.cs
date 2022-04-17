39c8ccf295422107c5c28613e31b5ae27891265c
One <One@DESKTOP-MIB22JC>
2022-04-17 19:58:08 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        byte room;
+        sbyte apples;
+        ushort students;
+        ulong costAuto;
+        int bankDebt;
+        bool checkpoint;
+        string name;
+        float coordinates;
+        double measurements;
+        decimal accurateMeasurements;
+        }
+    }
+
